1023056e22031594430cf56dca956164f533ec8f
Brandy CAYPE <brandycype234@gmail.com>
2023-03-13 16:45:16 +0300

    Implemented radiolisttile selection

--- lib/screens/time.dart
+++ lib/screens/time.dart
@@ -9,66 +9,98 @@ class Time extends StatefulWidget {
 }
 
 class _TimeState extends State<Time> {
+  late int selectedRadioTile;
   @override
+  void initState() {
+    super.initState();
+    selectedRadioTile = 0;
+  }
+setSelectedRadioTile(int val){
+setState((){
+  selectedRadioTile=val;
+  }
+
+  );
+}
+@override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-       const  Text('CHOOSE A CHECK-IN TIME',                style: TextStyle(color: AppColors.buttonText)),
-
-       const Text('A chat a day keeps the mind free',                style: TextStyle(color: AppColors.buttonText)),
-
+        const Text('CHOOSE A CHECK-IN TIME',
+            style: TextStyle(color: AppColors.buttonText)),
+        const Text('A chat a day keeps the mind free',
+            style: TextStyle(color: AppColors.buttonText)),
         RadioListTile(
           value: 1,
-          groupValue: 0,
-          onChanged: (val) => {print('Almost there $val')},
-          title:  const Text('Morning',                style: TextStyle(color: AppColors.buttonText)),
+          groupValue: selectedRadioTile,
+          onChanged: (val) => {
+          setSelectedRadioTile(val!),
+          },
+          title: const Text('Morning',
+              style: TextStyle(color: AppColors.buttonText)),
 
-          subtitle:  const Text('6:00 AM - 10: AM',                style: TextStyle(color: AppColors.buttonText)),
-
-          // activeColor: Colors.blue,
-        ),
-          RadioListTile(
-          value: 1,
-          groupValue: 0,
-          onChanged: (val) => {print('Almost there $val')},
-          title:  const Text('Day',                style: TextStyle(color: AppColors.buttonText)),
-          subtitle:  const Text('10:00 AM - 5:00 PM',                style: TextStyle(color: AppColors.buttonText)),
-          // activeColor: Colors.blue,
-        ),
-          RadioListTile(
-          value: 1,
-          groupValue: 0,
-          onChanged: (val) => {print('Almost there $val')},
-          title:  const Text('Evening',                style: TextStyle(color: AppColors.buttonText)),
-          subtitle:  const Text('5:00 PM - 8:00 PM',                style: TextStyle(color: AppColors.buttonText)),
+          subtitle: const Text('6:00 AM - 10: AM',
+              style: TextStyle(color: AppColors.buttonText)),
+          selected:true,
 
           // activeColor: Colors.blue,
         ),
-          RadioListTile(
-          value: 1,
-          groupValue: 0,
-          onChanged: (val) => {print('Almost there $val')},
-          title:  const Text('Night',                style: TextStyle(color: AppColors.buttonText)),
-          subtitle:  const Text('8:00 PM - 11: PM',                style: TextStyle(color: AppColors.buttonText)),
+        RadioListTile(
+          value: 2,
+          groupValue: selectedRadioTile,
+          onChanged: (val) => {
+          setSelectedRadioTile(val!)},
+          title:
+              const Text('Day', style: TextStyle(color: AppColors.buttonText)),
+          subtitle: const Text('10:00 AM - 5:00 PM',
+              style: TextStyle(color: AppColors.buttonText)),
+          // activeColor: Colors.blue,
+                    selected:false,
+
+        ),
+        RadioListTile(
+          value: 3,
+          groupValue: selectedRadioTile,
+          onChanged: (val) => {
+          setSelectedRadioTile(val!)},
+          title: const Text('Evening',
+              style: TextStyle(color: AppColors.buttonText)),
+          subtitle: const Text('5:00 PM - 8:00 PM',
+              style: TextStyle(color: AppColors.buttonText)),
+          selected:false,
+
           // activeColor: Colors.blue,
         ),
-         Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextButton(
-                  // ignore: avoid_print
-                  onPressed: () async => print('Yey!'),
-                  style: TextButton.styleFrom(
-                      backgroundColor: AppColors.button,
-                      fixedSize: const Size(227, 61),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0))),
+        RadioListTile(
+          value: 4,
+          groupValue: selectedRadioTile,
+          onChanged: (val) => {
+          setSelectedRadioTile(val!)},
+          title: const Text('Night',
+              style: TextStyle(color: AppColors.buttonText)),
+          subtitle: const Text('8:00 PM - 11: PM',
+              style: TextStyle(color: AppColors.buttonText)),
+          // activeColor: Colors.blue,
+                    selected:false,
 
-                  child: const Text('Next',
-                      style: TextStyle(color: AppColors.buttonText)),
-                )),
+        ),
+        Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              // ignore: avoid_print
+              onPressed: () async => print('Yey!'),
+              style: TextButton.styleFrom(
+                  backgroundColor: AppColors.button,
+                  fixedSize: const Size(227, 61),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0))),
+
+              child: const Text('Next',
+                  style: TextStyle(color: AppColors.buttonText)),
+            )),
       ],
     ));
   }
