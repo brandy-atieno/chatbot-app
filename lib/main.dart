e7328e97a306e28b89102435be50af5bb8c222c9
Brandy CAYPE <brandycype234@gmail.com>
2023-03-27 18:26:18 +0300

    Set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Chat Bot Application',
+        title: 'Chat  Application',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           scaffoldBackgroundColor: AppColors.primary,
